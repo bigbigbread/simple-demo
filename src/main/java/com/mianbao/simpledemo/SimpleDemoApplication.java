331815c5c05ec16e5e2743e5337149b47fec20db
bigbigbread <XYHxyh95@outlook.com>
2024-09-13 23:17:13 +0800

    init

---
+++ src/main/java/com/mianbao/simpledemo/SimpleDemoApplication.java
@@ -0,0 +1,25 @@
+package com.mianbao.simpledemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping
+@Slf4j
+public class SimpleDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleDemoApplication.class, args);
+    }
+    
+    @GetMapping("/hello")
+    public String f() {
+        String helloWorld = "Hello World!";
+        log.info(helloWorld);
+        return helloWorld;
+    }
+}
