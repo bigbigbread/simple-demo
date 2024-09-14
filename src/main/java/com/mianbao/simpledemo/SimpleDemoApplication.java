675ecb74c19e10a74d33dfc5687432687e7bc0d3
bigbigbread <XYHxyh95@outlook.com>
2024-09-14 18:25:02 +0800

    测试webhook 3

--- src/main/java/com/mianbao/simpledemo/SimpleDemoApplication.java
+++ src/main/java/com/mianbao/simpledemo/SimpleDemoApplication.java
@@ -23,9 +23,9 @@ public class SimpleDemoApplication {
         return helloWorld;
     }
     
-    @GetMapping("/hello2")
+    @GetMapping("/hello3")
     public String hello1() {
-        String hello1 = "hello2";
+        String hello1 = "hello3";
         log.info(hello1);
         return hello1;
     }
